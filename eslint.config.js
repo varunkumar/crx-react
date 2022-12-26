167f6efdbc0f9f50204d28ccd679e8e5adb1d419
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2022-12-26 23:13:07 +0530

    eslint config upgrade

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import typescript from '@typescript-eslint/eslint-plugin/recommended.js';
+import react from 'eslint-plugin-react/config/recommended.js';
+
+console.log(typescript);
+console.log(react);
+
+export default ['eslint:recommended', react, typescript];
